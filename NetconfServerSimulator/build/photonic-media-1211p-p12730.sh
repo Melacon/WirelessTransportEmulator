4ca6373dd30df7104e1c95d7713002d3e94ef44f
demx8as6 <martin.skorupski@highstreet-technologies.com>
2018-10-29 17:52:48 +0100

    fix xml filename

--- NetconfServerSimulator/build/photonic-media-1211p-p12730.sh
+++ NetconfServerSimulator/build/photonic-media-1211p-p12730.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./NetconfServerSimulator.sh ../xmlNeModel/photonic-media-1211p 12730 ../yang/yangNeModel
+./NetconfServerSimulator.sh ../xmlNeModel/photonic-media-1211p.xml 12730 ../yang/yangNeModel
